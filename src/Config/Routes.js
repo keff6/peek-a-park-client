ea696c657e100c9e1d82d95af1f77507c839e1cf
keff6 <kev.fallas@gmail.com>
2022-10-14 10:22:59 -0600

    Map added

--- src/Config/Routes.js
+++ src/Config/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Routes as Switch } from 'react-router-dom';
 import Landing from '../Landing/LandingPage';
+import ParksList from '../Parks/ParksList/ParksList';
 import StateSelection from '../StateSelection/StateSelection';
 
 const Routes = () => {
 const routes = (
   <Switch>
+    <Route exact path="/parks" element={<ParksList />}/>
     <Route exact path="/map" element={<StateSelection />}/>
     <Route exact path="/" element={<Landing />}/>
   </Switch>
--- src/Map/Map.jsx
+++ src/Map/Map.jsx
@@ -3,9 +3,24 @@ import { withStyles } from '@material-ui/core';
 import USAMap from 'react-usa-map';
 import styles from './Map.styles'
 
-const Map = ({classes}) => {
+const Map = ({
+  classes,
+  selectedState = '',
+  setSelectedState = () => null
+}) => {
   const mapHandler = (event) => {
     console.log(event.target.dataset.name);
+    setSelectedState(event.target.dataset.name)
+  };
+
+  const statesCustomConfig = () => {
+    if(!selectedState) return null
+
+    return {
+      [selectedState]: {
+        fill: "#fb8319",
+      }
+    };
   };
 
   return (
@@ -13,6 +28,7 @@ const Map = ({classes}) => {
       <USAMap 
         onClick={mapHandler}
         defaultFill='rgb(255,255,255,0.4)'
+        customize={statesCustomConfig()}
       />
     </div>
   )
